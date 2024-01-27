6a8caefbef316d6e1cd13494f39cccb131e5cd69
singanavena <parameshsp032@gmail.com>
2024-01-27 21:15:53 +0530

    update

---
+++ css/javascript/parameshindex.js
@@ -0,0 +1,59 @@
+// // console.log(a)
+// // var a = 20;
+// // var a = 50;
+// // let b = 20;
+// // b = 50;
+// // console.log(b)
+// //  const c = 20;
+// // // console.log(c)
+ 
+// // var a = 55;
+// // console.log(a)
+// // function add()
+// // {
+// //     if (true)
+// //     {
+// //         var pt = 50;
+// //     }
+// //     console.log(pt)
+// // }
+// // add()
+// // debugger;
+// // console.log(p1)
+// // var p1 = 55;
+// // let a = 63;
+// // const c = 65;
+// // console.log(a,c)
+// // function fun ()
+// // {
+// //     console.log(b)
+// //     var b = 55;
+// //     console.log(b)
+// // }
+// // fun()
+// // let a = 55;
+// // console.log(typeof(a))
+
+// let p=10;
+// var q=20;
+// const r=30;
+// p=15
+// var q=30;
+// q=35;
+// console.log(p,q,r)
+// console.log(typeof(p))
+// let a = 2.3666
+// console.log(a.toFixed(3))
+// console.log(a.toPrecision(3))
+// console.log(a.toString(8))
+
+
+const a = 1, b = -2, c = 1;
+let x1 = (-b+Math.sqrt(b*b-4*a*c)) (2*a)
+let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) (2*a) 
+console.log("x1=" +x1, "x2=" + x2)
+let str1 =("the time is 12'0 clock") 
+let str2 =('the kalam said "do it"')
+console.log(str1)
+console.log(str2)
+
